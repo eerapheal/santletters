e969074226646a9b86255e9de1b4fe93ec5cb0c0
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-03-19 23:12:25 +0530

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sant News Letters",
+  description: "Sant News Letters allows users to build codeless customs news letter email templete and send unlimited emails to subcribers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
