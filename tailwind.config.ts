3b9fe4095f37fec6eda1c47a93d0403fbfec5b05
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-03-19 23:49:07 +0530

    set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        display: ['Inter', 'system-ui', 'sans-serif'],
+        body: ['Inter', 'system-ui', 'sans-serif'],
+      },
+        colors: {
+        primaryColor: "#223740",
+        headingColor: "#223740",
+        smallTextColor: "#6b1d51"
+        },
     },
   },
   plugins: [],
