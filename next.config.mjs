da3cd37258d0ac1ae6ba8067f35a6b7935f2892f
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-04-03 16:27:57 +0530

    image domain

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,14 @@
 const nextConfig = {
   images: {
     domains: [
-      'resumeworded.com',
-      'seekvectorlogo.com',
-      'mumbrella.com.au',
-      'pngplay.com',
-      'img.clerk.com',
+      "resumeworded.com",
+      "seekvectorlogo.com",
+      "mumbrella.com.au",
+      "pngplay.com",
+      "thumbs.dreamstime.com",
+      "th.bing.com",
+      "as1.ftcdn.net",
+      "img.clerk.com",
     ],
   },
 };
