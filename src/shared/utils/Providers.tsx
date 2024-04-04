332daa333725b834f373027a876ca3226833c9b4
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-04-04 18:42:02 +0530

    user session

--- src/shared/utils/Providers.tsx
+++ src/shared/utils/Providers.tsx
@@ -1,23 +1,31 @@
 "use client";
-import { NextUIProvider } from '@nextui-org/react';
-import { usePathname } from 'next/navigation';
-
+import { NextUIProvider } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
 interface ProviderProps {
   children: React.ReactNode;
 }
 
-export default function Providers({children}: ProviderProps) {
+export default function Providers({ children }: ProviderProps) {
   const pathname = usePathname();
+  const { isLoaded } = useUser();
+  if (!isLoaded) {
+    return null;
+  };
 
   return (
     <NextUIProvider>
-      {pathname !== '/dashboard/new-email' && pathname !== '/' && pathname !== '/sign-in' && pathname !== '/subscribe' && pathname !== '/sign-up'? (
-        <div className='w-full flex'>
+      {pathname !== "/dashboard/new-email" &&
+      pathname !== "/" &&
+      pathname !== "/sign-in" &&
+      pathname !== "/subscribe" &&
+      pathname !== "/sign-up" ? (
+        <div className="w-full flex">
           <div className="w-[290px] h-screen overflow-y-scroll"></div>
         </div>
       ) : (
-        <>{children}</>
+        <>{children}</> 
       )}
     </NextUIProvider>
-  )
+  );
 }
