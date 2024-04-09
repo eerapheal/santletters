27a0da0432b91d3c20ea1ea682c45365c44f47d3
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-04-09 23:57:50 +0530

    User image

--- src/shared/widgets/header/ToolBar.tsx
+++ src/shared/widgets/header/ToolBar.tsx
@@ -1,17 +1,32 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
 import { Button } from "@nextui-org/react";
+import Image from "next/image";
 import Link from "next/link";
 
 const ToolBar = () => {
+  const { user } = useUser();
+
   return (
     <div className="flex items-center gap-3">
       <Button color="primary" className="font-[400] md:font-[500] text-lg">
         Start Trial
       </Button>
-      <Link href={"/sign-up"} className="font-[400] md:font-[500] text-xl md:text-lg">
-        Login
-      </Link>
+      {user ? (
+        <>
+          <Link href={"/dashboard"} className="rounded-full">
+            <Image src={user?.imageUrl} alt="profile" width={70} height={70} />
+          </Link>
+        </>
+      ) : (
+        <Link
+          href={"/sign-up"}
+          className="font-[400] md:font-[500] text-xl md:text-lg"
+        >
+          Login
+        </Link>
+      )}
     </div>
   );
 };
