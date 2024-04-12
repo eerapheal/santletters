a0770e05d7d8c9f98446fc855e2911ea832c51be
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-04-12 18:09:06 +0530

    dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Dashboard from '@/src/modules/dashboard/dashboard';
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <Dashboard />
+    </div>
+  )
+}
+
+export default Page;
