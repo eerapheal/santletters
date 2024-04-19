373c16f0cb01af555ba623dcd99dff49564b280d
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-04-19 22:13:15 +0530

    tutorial section

--- src/modules/dashboard/elements/main/main.tsx
+++ src/modules/dashboard/elements/main/main.tsx
@@ -6,6 +6,7 @@ import { Button } from "@nextui-org/react";
 import { useUser } from "@clerk/nextjs";
 import toast from "react-hot-toast";
 import { useState } from "react";
+import Link from "next/link";
 
 const Main = () => {
   const { user } = useUser();
@@ -71,10 +72,51 @@ const Main = () => {
               </div>
             </div>
           </div>
+          {/* tutorials */}
+          <div className="w-full bg-white border rounded p-5 my-3">
+            <h5 className="font-medium">Tutorials</h5>
+            <p className="text-sm opacity-[.7]">
+              Learn how to get started on Santnewsletters and utilize all our
+              features, directly from the Santnewsletters team.
+            </p>
+            <br />
+            <Button className="bg-[#FBCFE8] text-[#831743] rounded-lg h-[35px] flex items-center">
+              Tutorials <span>{ICONS.link}</span>
+            </Button>
+          </div>
+
+          {/* Need help? */}
+          <div className="w-full bg-white border rounded p-5 my-3">
+            <h5 className="font-medium">Need help?</h5>
+            <Link href={"/"}>
+              <div className="w-max px-3 my-2 h-[33px] bg-transparent border rounded-lg flex items-center">
+                <span className="text-sm">Knowledge base</span>
+                <span className="ml-1">{ICONS.link}</span>
+              </div>
+            </Link>
+            <Link href={"/"}>
+              <div className="w-max px-3 my-2 h-[33px] bg-transparent border rounded-lg flex items-center">
+                <span className="text-sm">API Documentation</span>
+                <span className="ml-1">{ICONS.link}</span>
+              </div>
+            </Link>
+            <Link href={"/"}>
+              <div className="w-max px-3 my-2 h-[33px] bg-transparent border rounded-lg flex items-center">
+                <span className="text-sm">Blog</span>
+                <span className="ml-1">{ICONS.link}</span>
+              </div>
+            </Link>
+            <Link href={"/"}>
+              <div className="w-max px-3 my-2 h-[33px] bg-transparent border rounded-lg flex items-center">
+                <span className="text-sm">FAQ</span>
+                <span className="ml-1">{ICONS.link}</span>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Main; 
+export default Main;
