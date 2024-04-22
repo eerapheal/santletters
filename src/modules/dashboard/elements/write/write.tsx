d064294303ec0cf7e21e0850a5a8bcba9cd6d1ee
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-04-22 23:58:59 +0530

    create new UI

--- src/modules/dashboard/elements/write/write.tsx
+++ src/modules/dashboard/elements/write/write.tsx
@@ -1,7 +1,21 @@
+"use client";
+import { useState } from "react";
 const Write = () => {
-return  (
-    <div>Write</div>
-)
-}
+  const [emailTittle, setEmailTitle] = useState(" ");
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full p-5 flex-wrap gap-6 relative">
+      <div
+        className="w-[200px] h-200px] bg-slate-50 flex flex-col items-center justify-center rounded border cursor-pointer"
+        onClick={() => setOpen(!open)}
+      >
+        <span className="text-2xl block text-center mb-3">
+          <h5 className="text-2xl">Create New</h5>
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Write;
