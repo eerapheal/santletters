9ec78aa12007f32b3250eeb4fb047469f0047a1b
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-05-04 23:51:06 +0530

    home add tto public route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/sign-in", "/sign-up", "/api/webhook", "/api/subscribe"],
+  publicRoutes: ["/", "/sign-in", "/sign-up", "/api/webhook", "/api/subscribe"],
 });
 
 export const config = {
