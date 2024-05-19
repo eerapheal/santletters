b061420dde5b2ed662378da5a45812cfed3c0817
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-05-19 05:35:09 +0530

    user public url

--- src/modules/dashboard/elements/main/main.tsx
+++ src/modules/dashboard/elements/main/main.tsx
@@ -62,7 +62,8 @@ const Main = () => {
                       copied ? "bg-blue-200" : "bg-transparent"
                     }`}
                   >
-                    https://www.linkedin.com/in/ekpenisiraphael/
+                    {process.env.NEXT_PUBLIC_URL}/subscribe?username=
+                    {user?.username}
                   </small>
                   <div className="absolute h-[38px] w-[90px] rounded-r-lg bg-[#DFE7FF] right-0 flex items-center justify-center">
                     <span className="text-lg">{ICONS.copy}</span>
